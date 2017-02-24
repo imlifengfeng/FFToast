78cc16b2c49103df5848f71e3da368eae8be0190
imlifengfeng <imlifengfeng@gmail.com>
2017-02-24 11:42:42 +0800

    Add FFCentreToastView

---
+++ FFToast/Assets/FFCentreToastView.m
@@ -0,0 +1,15 @@
+//
+//  FFCentreToastView.m
+//  FFToastDemo
+//
+//  Created by 李峰峰 on 2017/2/24.
+//  Copyright © 2017年 李峰峰. All rights reserved.
+//
+
+#import "FFCentreToastView.h"
+
+@implementation FFCentreToastView
+
+
+
+@end
